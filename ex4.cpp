ad56c8f6a0bc8d4527187872f912546c01e1cd47
jiaying37 <10911022@student.kjsh.ntpc.edu.tw>
2022-03-10 10:46:32 +0000

    update

---
+++ ex4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+     int a, b;
+     printf("A=> ");
+     scanf("%d", &a);
+     printf("B=> ");
+     scanf("%d", &b);
+     printf("A*A+B*B=%d\n)", a*a+b*b);
+     return 0;
+}
