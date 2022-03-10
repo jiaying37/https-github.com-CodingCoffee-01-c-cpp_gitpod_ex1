67beff9ba6c45b63fd240aa528e5f79c67dcab13
jiaying37 <10911022@student.kjsh.ntpc.edu.tw>
2022-03-10 11:01:07 +0000

    update

---
+++ ex5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+     float a, b;
+     printf("A=> ");
+     scanf("%f", &a);
+     printf("B=> ");
+     scanf("%f", &b);
+     printf("A*2*3.14=%f\n)", a*2*3.14);
+     printf("B*B*3.14=%f\n", b*b*3.14);
+     return 0;
+}
