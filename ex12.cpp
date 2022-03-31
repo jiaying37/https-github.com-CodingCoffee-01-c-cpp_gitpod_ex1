25ecda2e801efa0ae18c39e86957e741689f7741
jiaying37 <10911022@student.kjsh.ntpc.edu.tw>
2022-03-31 11:12:22 +0000

    update

---
+++ ex12.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main(){
+	   int a, b;
+     printf("your age A=> ");
+       scanf("%d", &a);
+     printf("firend age B=> ");
+       scanf("%d", &b);
+    if (a > b ){
+        printf("  %d \n ",a);
+    }else{
+        printf("  %d \n",b);
+    }
+       
+    
+}
