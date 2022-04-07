de805cd51d748d5cf573173871aadf1fa0120fac
jiaying37 <10911022@student.kjsh.ntpc.edu.tw>
2022-04-07 11:20:35 +0000

    update

---
+++ ex20.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i <= 7 ; i++){
+        for( j=1;j <= i ; j++){
+        
+           printf("%d",j);
+        }
+        cout << endl ;
+        
+    }
+     return 0;
+}
+   
