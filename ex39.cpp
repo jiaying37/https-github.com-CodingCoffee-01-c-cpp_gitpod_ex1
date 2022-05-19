d39e900f1b6c68fad047332ed35aa4e76bc32ee0
jiaying37 <10911022@student.kjsh.ntpc.edu.tw>
2022-05-19 10:38:28 +0000

    update

---
+++ ex39.cpp
@@ -0,0 +1,33 @@
+
+using namespace std;
+
+void print_array(int* a){
+    int i;
+    
+    for( i = 0;i <  6;i++);{
+        printf("a[%d] =%d ",i,a[i]);
+    }
+}
+
+int main()
+{
+    int b,a[6]={3,5,8,67,54,21};
+    int* p;
+    
+    printf("please input a num:");
+    scanf("%d",&b);
+    p = &b;
+    
+    printf("*p = %d\n",*p);
+    printf("p = %x\n",p);
+    printf("&b = %x\n",&b);
+    
+    p = a;
+    for(int i =0;i < 6;i++){
+        printf("%x ",p+i);
+    }
+    printf("\n");
+     print_array(a);
+    
+    return 0;
+}
