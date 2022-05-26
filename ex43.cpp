67a1deae0c913c14df2484dafbc7f8405856c9d0
jiaying37 <10911022@student.kjsh.ntpc.edu.tw>
2022-05-26 10:33:23 +0000

    update

---
+++ ex43.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int a[10];
+    for(int i=0;i<10;i++)
+    cin>>a[i];
+    sort(a,a+10);
+    for(int i=9;i>0;i--)
+    cout<<a[i]<<" ";
+    cout<<a[0]<<endl;
+ }
